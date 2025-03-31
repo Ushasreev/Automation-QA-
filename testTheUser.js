fc2a13c13924fbb06919afc69925ee223a4e381b
Ushasreev <140659131+Ushasreev@users.noreply.github.com>
2025-03-31 13:22:49 -0400

    Create testTheUser.js

---
+++ testTheUser.js
@@ -0,0 +1,10 @@
+// Create a new GET request:
+// https://gorest.co.in/public/v2/users/{{user_id}}
+// In the Tests tab, add assertions 
+pm.test("User exists", function () {
+    pm.response.to.have.status(200);
+    var jsonData = pm.response.json();
+    pm.expect(jsonData).to.have.property("id");
+    pm.expect(jsonData).to.have.property("name", "Test User");
+    pm.expect(jsonData).to.have.property("email");
+});
