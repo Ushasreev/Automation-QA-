6c3cd2a4d2d557a9a4b1da69de7cf19eeddf8d05
Ushasreev <140659131+Ushasreev@users.noreply.github.com>
2025-03-31 14:12:49 -0400

    Update and rename 2b) testTheUser.js to 1b) testTheUser.js

--- 1b) testTheUser.js
+++ 1b) testTheUser.js
@@ -1,5 +1,5 @@
 // Create a new GET request:
-// https://gorest.co.in/public/v2/users/{{user_id}}
+// https://gorest.co.in/public/v2/users/7806878
 // In the Tests tab, add assertions 
 pm.test("User exists", function () {
     pm.response.to.have.status(200);
